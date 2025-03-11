f144ac5ed0a98cda0407cb2d52801af978efa36a
Tijesunimi Peters <tijpeters@paypal.com>
2025-03-11 22:17:21 +0000

    Adds configs and plugins folder

--- init.lua
+++ init.lua
@@ -1,17 +1,4 @@
--- local path_package = vim.fn.stdpath('data') .. '/site'
--- local mini_path = path_package .. '/pack/deps/start/mini.nvim'
--- if not vim.loop.fs_stat(mini_path) then
---   vim.cmd('echo "Installing `mini.nvim`" | redraw')
---   local clone_cmd = {
---     'git', 'clone', '--filter=blob:none',
---     -- Uncomment next line to use 'stable' branch
---     -- '--branch', 'stable',
---     'https://github.com/echasnovski/mini.nvim', mini_path
---   }
---   vim.fn.system(clone_cmd)
---   vim.cmd('packadd mini.nvim | helptags ALL')
--- end
-
+require("config.lazy")
 --vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 --vim.g.mapleader = " "
 
